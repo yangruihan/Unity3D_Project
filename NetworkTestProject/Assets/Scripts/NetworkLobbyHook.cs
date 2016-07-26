4ccecbdb9efab9c6b4b4668b178b5ccd7ac31c70
C.y <coderyrh9236@gmail.com>
2016-07-26 22:50:20 +0800

    添加"NetworkTestProject"

---
+++ NetworkTestProject/Assets/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prototype.NetworkLobby;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook : LobbyHook {
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        Player player = gamePlayer.GetComponent<Player>();
+
+        player.playerName = lobby.playerName;
+        player.playerColor = lobby.playerColor;
+    }
+}
---
+++ NetworkTestProject/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player : NetworkBehaviour
+{
+
+    public float speed = 2.0f;
+
+    [SyncVar]
+    public Color playerColor = Color.white;
+    [SyncVar]
+    public string playerName;
+
+    protected Rigidbody2D _rigidbody;
+
+    protected float _horizontal = 0;
+    protected float _vertical = 0;
+    protected float _inverseMoveTime = 0.1f;
+    protected Vector2 _targetPos;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        Renderer[] rends = GetComponentsInChildren<Renderer>();
+        foreach (var r in rends)
+        {
+            r.material.color = playerColor;
+        }
+    }
+
+    [ClientCallback]
+    void Update()
+    {
+        _horizontal = 0;
+        _vertical = 0;
+
+        if (!isLocalPlayer)
+            return;
+
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    [ClientCallback]
+    void FixedUpdate()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        _rigidbody.MovePosition(Vector2.Lerp(transform.position, _targetPos, _inverseMoveTime * Time.deltaTime));
+
+        if (_horizontal != 0 || _vertical !=0)
+        {
+            _targetPos += new Vector2(_horizontal, _vertical);
+        }
+    }
+}
