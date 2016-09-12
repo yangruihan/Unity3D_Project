9d30a14426537b41d360492dc5c862700fd63671
C.y <coderyrh9236@gmail.com>
2016-09-12 21:31:46 +0800

    添加 uGUI_Study

---
+++ uGUI_Study/Assets/InteractionComponents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionComponents : MonoBehaviour {
+
+    public void OnToggleValueChanged(bool value)
+    {
+        Debug.Log(value);
+    }
+
+    public void OnDropdownValueChange(int id)
+    {
+        Debug.Log(id);
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("<color=green>Hello</color>");
+    }
+}
