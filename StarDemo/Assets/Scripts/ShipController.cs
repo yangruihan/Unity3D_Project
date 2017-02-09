51027ab35571c16779a8171053b38e409a775d93
C.y <yangruihan@vip.qq.com>
2017-02-09 20:30:52 +0800

    "Add StarDemo"

---
+++ StarDemo/Assets/Scripts/ShipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ShipController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveForce;		// 移动牵引力
+
+	private Rigidbody2D rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		float x = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
+		float y = CrossPlatformInputManager.GetAxisRaw ("Vertical");
+
+		Vector2 forceDir = new Vector2 (x, y).normalized;
+		rigidbody.AddForce (forceDir * moveForce);
+	}
+}
---
+++ StarDemo/Assets/Scripts/StarGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarGravity : MonoBehaviour {
+
+	[SerializeField]
+	private float gravityValue;		// 引力大小
+
+	private Rigidbody2D role;
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag == "ship" && (role == null || role.transform != other.transform)) {
+			role = other.GetComponent<Rigidbody2D> ();
+		}
+
+		var direction = (transform.position - role.transform.position).normalized;
+		role.AddForce (direction * gravityValue);
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		role = null;
+	}
+}
